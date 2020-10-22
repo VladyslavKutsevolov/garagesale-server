6870ec73dc88b5f98510616a4ea8ef761fd06f9f
JaeyoungKim <jzero.youngs89@gmail.com>
2020-10-22 05:14:37 +0000

    Create route

--- src/server.js
+++ src/server.js
@@ -5,15 +5,15 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const api = require('./routes/index.js')
+
 
 app.use(cors());
 app.use(helmet());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send({message: 'Hello Express!'});
-});
+api.use('/api', api)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
