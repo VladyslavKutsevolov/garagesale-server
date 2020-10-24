723b4c4168d27e688f9ab6250121d91708fbbd10
JaeyoungKim <jzero.youngs89@gmail.com>
2020-10-24 08:41:45 +0000

    mock database

--- src/db/schema/schema.sql
+++ src/db/schema/schema.sql
@@ -1,11 +1,67 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS locations CASCADE;
 DROP TABLE IF EXISTS garage_sales CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS product_categories CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  to_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP NOT NULL,
+  message_text text NOT NULL,
+  read_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  postcode VARCHAR(50) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(50) NOT NULL,
+  longitude VARCHAR(255) NOT NULL,
+  latitude VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE garage_sales (
   id SERIAL PRIMARY KEY NOT NULL,
-  seller_id INTEGER NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  cover_photo_url VARCHAR(255),
-  createdAt DATE NOT NULL,
-  location_id INTEGER NOT NULL
+  cover_photo_url TEXT,
+  created_at DATE NOT NULL,
+  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  image_url TEXT,
+  price VARCHAR(255),
+  sold Boolean not NULL default false,
+  sale_id INTEGER REFERENCES garage_sales(id) ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product_categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
