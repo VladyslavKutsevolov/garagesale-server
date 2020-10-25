0508a7941c3a2a63606ac03926a1f0fe9ea08e49
Vladyslav <kytsevolov@gmail.com>
2020-10-25 12:46:46 -0600

    added morgan logger

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,37 +1,75 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const aws = require('aws-sdk');
+const S3_BUCKET = process.env.Bucket;
+const s3 = new aws.S3();
 
-const addNewProduct = function(item, db) {
+// AWS config
+aws.config.update({
+  accessKeyId: process.env.AWSAccessKeyId,
+  secretAccessKey: process.env.AWSSecretKey,
+});
+
+// upload image setup
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: S3_BUCKET,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const fileName = `${Date.now().toString()}-${file.originalname}`;
+      const path = `product-images/${fileName}`;
+      cb(null, path);
+    },
+  }),
+});
+
+const addNewProduct = function (item, db) {
   const queryString = `INSERT INTO products (title, description, image_url, price, sale_id) VALUES ($1, $2, $3, $4, $5);`;
 
-  const valueArray = [item.title, item.description, item.image_url, item.price, item.sale_id]
+  const valueArray = [
+    item.title,
+    item.description,
+    item.image_url,
+    item.price,
+    item.sale_id,
+  ];
 
-  return db.query(queryString, valueArray)
-    .then(data => data.rows)
+  return db.query(queryString, valueArray).then((data) => data.rows);
 };
 
-module.exports = db => {
-  router.get("/", (req, res) => {
+module.exports = (db) => {
+  router.get('/', (req, res) => {
     db.query(`SELECT * FROM products;`)
-      .then(data => {
-        const listOfProducts = data.rows
-        res.json({listOfProducts})
+      .then((data) => {
+        const listOfProducts = data.rows;
+        res.json({ listOfProducts });
       })
-      .catch(err => console.log('query Error', err))
+      .catch((err) => console.log('query Error', err));
   });
 
-  router.post("/new", (req, res) => {
-    const newItem = req.body;
+  router.post('/new', upload.single('product_img'), (req, res) => {
+    const parseBodyValues = JSON.parse(JSON.stringify(req.body));
+    const formFieldValues = {
+      ...parseBodyValues,
+      sale_id: 1,
+      image_url: req.file.location,
+    };
 
-    addNewProduct(newItem, db)
-    .then(message => {
-      return res.json( {message: "New item is added on your Garage!"} );
-    }).catch(err => {
-      console.log(err.message)
-      return res
-        .status(500)
-        .json({ error: err.message });
-    });
-  })
+    addNewProduct(formFieldValues, db)
+      .then((message) => {
+        return res.json({ message: 'New item is added on your Garage!' });
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.status(500).json({ error: err.message });
+      });
+  });
 
   return router;
 };
--- src/server.js
+++ src/server.js
@@ -2,28 +2,33 @@
 require('dotenv').config();
 
 // Web server config.
-const express = require("express");
-const bodyparser = require("body-parser");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express');
+const bodyparser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const cookieSession = require('cookie-session');
+const logger = require('morgan');
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['secret']
-}));
+app.use(logger('dev'));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['secret'],
+  })
+);
 
 // Database setup
-const db = require("./db/database");
+const db = require('./db/database');
 db.connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+  .then(() => console.log('DB connected'))
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 //Routes
 const sales = require('./routes/sales');
 const products = require('./routes/products');
-const usersRoutes = require("./routes/users");
+const usersRoutes = require('./routes/users');
 
 app.use(cors());
 app.use(helmet());
@@ -31,13 +36,13 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send({ message: 'hello Express!'})
+  res.send({ message: 'hello Express!' });
 });
 
 // Use routes modules
-app.use("/sales", sales(db));
-app.use("/products", products(db));
-app.use("/users", usersRoutes(db));
+app.use('/sales', sales(db));
+app.use('/products', products(db));
+app.use('/users', usersRoutes(db));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
