c31aa44f2a9c4ba6f68d972b378724c95e64e647
JaeyoungKim <jzero.youngs89@gmail.com>
2020-10-26 04:14:55 +0000

    chat mode practice

--- src/server.js
+++ src/server.js
@@ -6,10 +6,59 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
-const app = express();
+const http = require('http');
 const PORT = process.env.PORT || 3001;
 const cookieSession = require('cookie-session');
 const logger = require('morgan');
+const socketio = require('socket.io');
+const path = require('path');
+const formatMessage = require('../utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('../utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
+app.use(helmet());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+const botName = 'Chat Bot'
+
+io.on('connection', socket => {
+
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    //Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    // Broadcast when other user connects
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username} has joined the chat`));
+
+    //Run when client disconnect
+    socket.on('disconnect', () => {
+      io.emit('message', formatMessage(botName,`${user.username} has left the chat`))
+    });
+  })
+
+  // Listen for chat Messages
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id)
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  })
+
+});
 
 app.use(logger('dev'));
 app.use(
@@ -30,20 +79,11 @@ const sales = require('./routes/sales');
 const products = require('./routes/products');
 const usersRoutes = require('./routes/users');
 
-app.use(cors());
-app.use(helmet());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send({ message: 'hello Express!' });
-});
-
 // Use routes modules
 app.use('/sales', sales(db));
 app.use('/products', products(db));
 app.use('/users', usersRoutes(db));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
 });
