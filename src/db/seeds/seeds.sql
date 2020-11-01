ff85bfa41cf576266f51d5bd20c498db25561c95
Vladyslav <kytsevolov@gmail.com>
2020-11-01 15:19:26 -0700

    removed personal care from categories and added others

--- src/db/seeds/seeds.sql
+++ src/db/seeds/seeds.sql
@@ -42,10 +42,10 @@ values
   ('Books'),
   ('Toys'),
   ('Media'),
-  ('Personal Care'),
   ('Appliances'),
   ('Clothes'),
-  ('Tools');
+  ('Tools'),
+  ('Others');
 
 
 
@@ -57,13 +57,13 @@ values
   (2, 'GalaxyNote10', 'Duis at velit eu est congue elementum.', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-202005
 ?wid=892&hei=820&&qlt=80&.v=1587460552755', 251.75, true, 2, 3),
   (1, 'Ostrich - Prime Cut', 'Integer ac leo.', 'https://upload.wikimedia.org/wikipedia/commons/e/e1/Apple_Keyboard_with_Numeric_Keyboard_9612.jpg', 22.63, true, 1, 5),
-  (1,'Apple - Custard', 'Descrption',
+  (1, 'Apple - Custard', 'Descrption',
     'https://www.pngjoy.com/pngm/53/1199732_nike-shoe-men-s-nike-zoom-fly-flyknit.png', 14.33, false, 1, 2),
-  (2,'Wine - Cahors Ac 2000, Clos', 'Morbi ut odio.', 'https://www.superebikes.ca/wp-content/uploads/2020/03/e-wild-s-black-2-560x560.jpg', 11.44, true, 2, 6),
-  (1,'Lid - Translucent, 3.5 And 6 Oz', 'Etiam faucibus cursus urna. Ut tellus.', 'https://images.theconversation.com/files/107896/original/image-20160112-6996-1jahuzf.JPG?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 64.0, false, 1, 8),
-  (2,'Wine - Zinfandel Rosenblum', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 57.22, false, 2, 10),
-  (1,'Toy Hulk', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 20.75, false, 1, 6),
-  (3,'Motorola Phone', 'Never Used, It is foldable!', 'https://images.theconversation.com/files/107896/original/image-20160112-6996-1jahuzf.JPG?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 100.0, false, 3, 1),
-  (3,'Toy CyberTruck', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 57.22, false, 3, 5),
-  (3,'Smart TV', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat.', 'https://thumbs.dreamstime.com/z/television-monitor-texture-sky-isolated-white-background-54339475.jpg', 2000.75, false, 3, 1);
+  (2, 'Wine - Cahors Ac 2000, Clos', 'Morbi ut odio.', 'https://www.superebikes.ca/wp-content/uploads/2020/03/e-wild-s-black-2-560x560.jpg', 11.44, true, 2, 6),
+  (1, 'Lid - Translucent, 3.5 And 6 Oz', 'Etiam faucibus cursus urna. Ut tellus.', 'https://images.theconversation.com/files/107896/original/image-20160112-6996-1jahuzf.JPG?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 64.0, false, 1, 8),
+  (2, 'Wine - Zinfandel Rosenblum', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 57.22, false, 2, 10),
+  (1, 'Toy Hulk', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 20.75, false, 1, 6),
+  (3, 'Motorola Phone', 'Never Used, It is foldable!', 'https://images.theconversation.com/files/107896/original/image-20160112-6996-1jahuzf.JPG?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 100.0, false, 3, 1),
+  (3, 'Toy CyberTruck', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg', 57.22, false, 3, 5),
+  (3, 'Smart TV', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat.', 'https://thumbs.dreamstime.com/z/television-monitor-texture-sky-isolated-white-background-54339475.jpg', 2000.75, false, 3, 1);
 
