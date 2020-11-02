5106a7c3437fe67841ce8be34ae7cb91908773bd
JaeyoungKim <jzero.youngs89@gmail.com>
2020-11-02 06:50:23 +0000

    Validation for Garage 1102_0050

--- src/routes/users.js
+++ src/routes/users.js
@@ -66,7 +66,6 @@ module.exports = db => {
 
   router.post("/login", async (req, res) => {
     const user = req.body;
-    console.log('user data', user)
     try {
       const userExists = await checkUserExists(user, db);
       if (userExists) {
